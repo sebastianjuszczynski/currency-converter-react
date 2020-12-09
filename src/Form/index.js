b0042ad35384cd0c96d74925b682e546b951bb42
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-09 22:06:22 +0000

    code formatted and z-index changed in .container

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,27 +2,27 @@ import React from "react";
 import "./style.css";
 
 const Form = () => (
-    <form className="form">
+  <form className="form">
     <fieldset className="form__fieldset">
       <legend className="form__legend">Uzupełnij dane:</legend>
       <p>
         <label for="amount" className="form__label">Podaj kwotę w złotówkach*:</label>
         <input className="form__input" type="number" id="amount" name="amount" required />
       </p>
-        <p>
-          <label for="currency" className="form__label">Waluta:</label>
-          <select className="form__input" name="currency" id="currency">
-            <option value="GBP">Funt brytyjski</option>
-            <option value="EUR">Euro</option>
-            <option value="USD">Dolar amerykański</option>
-            <option value="CHF">Frank szwajcarski</option>
-          </select>
-        </p>
-        <p>
-          <button className="form__button">Przelicz</button>
-        </p>
-  </fieldset>
-</form>
+      <p>
+        <label for="currency" className="form__label">Waluta:</label>
+        <select className="form__input" name="currency" id="currency">
+          <option value="GBP">Funt brytyjski</option>
+          <option value="EUR">Euro</option>
+          <option value="USD">Dolar amerykański</option>
+          <option value="CHF">Frank szwajcarski</option>
+        </select>
+      </p>
+      <p>
+        <button className="form__button">Przelicz</button>
+      </p>
+    </fieldset>
+  </form>
 );
 
 export default Form;
