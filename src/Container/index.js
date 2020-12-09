eda747dbd2ae5033d2fa4048b6432f2320d0fd1b
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-09 22:07:01 +0000

    added children to Container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./style.css";
-import Form from "../Form";
-import Result from "../Result";
 
-const Container = () => (
-    <main className="container">
-      <h1 className="header">Kalkulator walut 💸</h1>
-      <Form />
-      <Result />
-    </main>
+
+const Container = ({ children }) => (
+  <main className="container">
+    {children}
+  </main>
 );
 
 export default Container;
