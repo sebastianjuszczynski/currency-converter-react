2ef7b0f866793c41e886a3cfde36930bece49095
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-09 21:29:26 +0000

    added result component

---
+++ src/Result/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Result = () => (
+    <>
+    <p className="result__info">Kursy walut - Tabela A kursów średnich walut obcych na podstawie notowań
+        Narodowego Banku Polskiego obowiązujące od dnia 2020-11-27</p>
+
+      <p className="result__paragraph">
+        Wpisz kwotę w PLN-ach a następnie kliknij w przycisk "PRZELICZ"
+    </p>
+    </>
+);
+
+export default Result;
