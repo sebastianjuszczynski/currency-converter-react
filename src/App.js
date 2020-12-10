a1177d6696fad5dc0c4d5d69ed8b5f4f2b1e9340
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-10 12:47:52 +0000

    submit interavtion partially finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from "./Form";
 import Result from "./Result";
 import Container from "./Container";
@@ -7,10 +7,17 @@ import Container from "./Container";
 
 
 function App() {
+
+  const [result, setNewResult] = useState('Wpisz kwotę w PLN-ach a następnie kliknij w przycisk "PRZELICZ"');
+
+  const resultUpdate = (newAmount) => {
+    setNewResult(result => result = `${newAmount} złotych to w przeliczeniu`);
+  };
+
   return (
     <Container>
-      <Form />
-      <Result />
+      <Form resultUpdate={resultUpdate} />
+      <Result result={result} />
     </Container>
   );
 }
--- src/Result/index.js
+++ src/Result/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./style.css";
 
-const Result = () => (
+const Result = ({ resultUpdate, result }) => (
   <>
     <p className="result__info">Kursy walut - Tabela A kursów średnich walut obcych na podstawie notowań
         Narodowego Banku Polskiego obowiązujące od dnia 2020-11-27</p>
 
-    <p className="result__paragraph">
-      Wpisz kwotę w PLN-ach a następnie kliknij w przycisk "PRZELICZ"
+    <p className="result__paragraph" onChange={resultUpdate}>
+  {result}
     </p>
   </>
 );
