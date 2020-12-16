fc26decea06dcd1241c1db20c9a974e04f28b004
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-12-16 17:45:09 +0000

    font-family changed to Nerko one

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Nerko One', cursive;
     background-color: #eee;
     }
 `;
